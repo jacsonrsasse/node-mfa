5cd346abe553a527b5e4b64081925ef3600fda18
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-02-02 02:00:23 -0300

    feat: improve error response

--- src/infra/http/fastify/response/with-error.ts
+++ src/infra/http/fastify/response/with-error.ts
@@ -2,5 +2,7 @@ import { IException } from '@shared/exceptions/exception';
 import { FastifyReply } from 'fastify';
 
 export function withError(response: FastifyReply, error: IException) {
-  return response.status(error.statusCode).send(error.message);
+  return response
+    .status(error.statusCode)
+    .send({ statusCode: error.statusCode, message: error.message });
 }

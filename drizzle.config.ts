bd9bbe02716e6b0a835c0628d2f5230e05f6eb30
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-02-03 00:01:13 -0300

    fix: drizzle config after folder changes

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'drizzle-kit'
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  schema: './src/infra/db/schema.ts',
-  out: './src/infra/db/migrations',
+  schema: './src/infra/db/drizzle/schema.ts',
+  out: './src/infra/db/drizzle/migrations',
   dialect: 'sqlite',
   dbCredentials: {
-    url: process.env.DB_FILE_NAME!
-  }
-})
+    url: process.env.DB_FILE_NAME!,
+  },
+});

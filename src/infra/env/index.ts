9d538122517561089d0e125fda1c818998b05faf
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-02-05 00:07:22 -0300

    feat: using zod to validate environment

---
+++ src/infra/env/index.ts
@@ -0,0 +1,20 @@
+import { configDotenv } from 'dotenv';
+import { exit } from 'process';
+import { z } from 'zod';
+
+configDotenv();
+
+const environmentSchema = z.object({
+  DB_FILE_NAME: z.string(),
+  HASHING_SECRET: z.string(),
+  ENCRYPT_SECRET: z.string(),
+});
+
+const { data: env, error } = environmentSchema.safeParse(process.env);
+if (error) {
+  console.error('Failed to parse environment!', process.env);
+  console.error(error.errors);
+  exit(1);
+}
+
+export { env };

38018afcc59c55e0f79bfe6e56a0bf832b506d47
Jacson R. Sasse <jacson.sasse@hotmail.com>
2025-02-13 00:19:46 -0300

    fix: if date is undefined returns null

--- src/shared/utils/date-to-sqlite-string.util.ts
+++ src/shared/utils/date-to-sqlite-string.util.ts
@@ -1,4 +1,6 @@
 export function dateToSqliteString(date: Date) {
+  if (!date) return null;
+
   const year = date.getFullYear();
   const month = date.getMonth().toString().padStart(2, '0');
   const day = date.getDate().toString().padStart(2, '0');
